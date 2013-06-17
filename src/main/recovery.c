d9b0515384b5afb13931878d1959e308d5f4b568
Muhammad Usama <muhammad.usama@enterprisedb.com>
2013-06-17 18:41:26 +0500

    initial version.

---
+++ src/main/recovery.c
@@ -0,0 +1,428 @@
+/* -*-pgsql-c-*- */
+/*
+ * $Header$
+ *
+ * pgpool: a language independent connection pool server for PostgreSQL
+ * written by Tatsuo Ishii
+ *
+ * Copyright (c) 2003-2013	PgPool Global Development Group
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby
+ * granted, provided that the above copyright notice appear in all
+ * copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of the
+ * author not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. The author makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * recovery.c: online recovery process
+ *
+ */
+
+#include "config.h"
+
+#include <unistd.h>
+#include <string.h>
+
+#include "pool.h"
+#include "pool_config.h"
+
+#include "libpq-fe.h"
+
+#include "watchdog/watchdog.h"
+#include "watchdog/wd_ext.h"
+
+#define WAIT_RETRY_COUNT (pool_config->recovery_timeout / 3)
+
+#define FIRST_STAGE 0
+#define SECOND_STAGE 1
+
+static int exec_checkpoint(PGconn *conn);
+static int exec_recovery(PGconn *conn, BackendInfo *backend, char stage);
+static int exec_remote_start(PGconn *conn, BackendInfo *backend);
+static PGconn *connect_backend_libpq(BackendInfo *backend);
+static int check_postmaster_started(BackendInfo *backend);
+
+static char recovery_command[1024];
+
+extern volatile sig_atomic_t pcp_wakeup_request;
+
+/*
+ * Start online recovery.
+ * "recovery_node" is the node to be recovered.
+ * Master or primary node is chosen in this function.
+ */
+int start_recovery(int recovery_node)
+{
+	int node_id;
+	BackendInfo *backend;
+	BackendInfo *recovery_backend;
+	PGconn *conn;
+	int failback_wait_count;
+#define FAILBACK_WAIT_MAX_RETRY 5		/* 5 seconds should be enough for failback operation */
+
+	pool_log("starting recovering node %d", recovery_node);
+
+	if (VALID_BACKEND(recovery_node))
+	{
+		pool_error("start_recovery: backend node %d is alive", recovery_node);
+		return 1;
+	}
+
+	Req_info->kind = NODE_RECOVERY_REQUEST;
+
+	/* select master/primary node */
+	node_id = MASTER_SLAVE ? PRIMARY_NODE_ID : REAL_MASTER_NODE_ID;
+	backend = &pool_config->backend_desc->backend_info[node_id];
+
+	/* get node info to be recovered */
+	recovery_backend = &pool_config->backend_desc->backend_info[recovery_node];
+
+	conn = connect_backend_libpq(backend);
+	if (conn == NULL)
+	{
+		pool_error("start_recovery: could not connect master node (%d)", node_id);
+		return 1;
+	}
+
+	/* 1st stage */
+	if (REPLICATION)
+	{
+		if (exec_checkpoint(conn) != 0)
+		{
+			PQfinish(conn);
+			pool_error("start_recovery: CHECKPOINT failed");
+			return 1;
+		}
+		pool_log("CHECKPOINT in the 1st stage done");
+	}
+
+	if (exec_recovery(conn, recovery_backend, FIRST_STAGE) != 0)
+	{
+		PQfinish(conn);
+		return 1;
+	}
+
+	pool_log("1st stage is done");
+
+	if (REPLICATION)
+	{
+		pool_log("starting 2nd stage");
+
+		/* 2nd stage */
+		*InRecovery = RECOVERY_ONLINE;
+		if (pool_config->use_watchdog)
+		{
+			/* announce start recovery */
+			if (WD_OK != wd_start_recovery())
+			{
+				PQfinish(conn);
+				pool_error("start_recovery: timeover for waiting connection closed in the other pgpools");
+				return 1;
+			}
+		}
+
+		if (wait_connection_closed() != 0)
+		{
+			PQfinish(conn);
+			pool_error("start_recovery: timeover for waiting connection closed");
+			return 1;
+		}
+
+		pool_log("all connections from clients have been closed");
+
+		if (exec_checkpoint(conn) != 0)
+		{
+			PQfinish(conn);
+			pool_error("start_recovery: CHECKPOINT failed");
+			return 1;
+		}
+
+		pool_log("CHECKPOINT in the 2nd stage done");
+
+		if (exec_recovery(conn, recovery_backend, SECOND_STAGE) != 0)
+		{
+			PQfinish(conn);
+			return 1;
+		}
+	}
+
+	if (exec_remote_start(conn, recovery_backend) != 0)
+	{
+		PQfinish(conn);
+		pool_error("start_recovery: remote start failed");
+		return 1;
+	}
+
+	if (check_postmaster_started(recovery_backend))
+	{
+		PQfinish(conn);
+		pool_error("start_recovery: check start failed");
+		return 1;
+	}
+
+	pool_log("%d node restarted", recovery_node);
+
+	/*
+	 * reset failover completion flag.  this is necessary since
+	 * previous failover/failback will set the flag to 1.
+	 */
+	pcp_wakeup_request = 0;
+
+	/* send failback request to pgpool parent */
+	send_failback_request(recovery_node);
+
+	/* wait for failback */
+	failback_wait_count = 0;
+	while (!pcp_wakeup_request)
+	{
+		struct timeval t = {1, 0};
+		/* polling SIGUSR2 signal every 1 sec */
+		select(0, NULL, NULL, NULL, &t);
+		failback_wait_count++;
+		if (failback_wait_count >= FAILBACK_WAIT_MAX_RETRY)
+		{
+			pool_log("start_recovery: waiting for wake up request is timeout(%d seconds)",
+					 FAILBACK_WAIT_MAX_RETRY);
+			break;
+		}
+	}
+	pcp_wakeup_request = 0;
+
+	PQfinish(conn);
+
+	pool_log("recovery done");
+
+	return 0;
+}
+
+/*
+ * Notice all children finishing recovery.
+ */
+void finish_recovery(void)
+{
+	/* announce end recovery */
+	if (pool_config->use_watchdog && *InRecovery != RECOVERY_INIT)
+	{
+		wd_end_recovery();
+	}
+
+	*InRecovery = RECOVERY_INIT;
+	kill(getppid(), SIGUSR2);
+}
+
+/*
+ * Execute CHECKPOINT
+ */
+static int exec_checkpoint(PGconn *conn)
+{
+	PGresult *result;
+	int r;
+
+	pool_debug("exec_checkpoint: start checkpoint");
+	result = PQexec(conn, "CHECKPOINT");
+	r = (PQresultStatus(result) !=  PGRES_COMMAND_OK);
+	PQclear(result);
+	pool_debug("exec_checkpoint: finish checkpoint");
+	return r;
+}
+
+/*
+ * Call pgpool_recovery() function.
+ */
+static int exec_recovery(PGconn *conn, BackendInfo *backend, char stage)
+{
+	PGresult *result;
+	char *hostname;
+	char *script;
+	int r;
+
+	if (strlen(backend->backend_hostname) == 0 || *(backend->backend_hostname) == '/')
+		hostname = "localhost";
+	else
+		hostname = backend->backend_hostname;
+
+	script = (stage == FIRST_STAGE) ?
+		pool_config->recovery_1st_stage_command : pool_config->recovery_2nd_stage_command;
+
+	if (script == NULL || strlen(script) == 0)
+	{
+		/* do not execute script */
+		return 0;
+	}
+
+	snprintf(recovery_command,
+			 sizeof(recovery_command),
+			 "SELECT pgpool_recovery('%s', '%s', '%s')",
+			 script,
+			 hostname,
+			 backend->backend_data_directory);
+
+	pool_log("starting recovery command: \"%s\"", recovery_command);
+
+	pool_debug("exec_recovery: start recovery");
+	result = PQexec(conn, recovery_command);
+	r = (PQresultStatus(result) !=  PGRES_TUPLES_OK);
+	if (r != 0)
+	{
+		pool_error("exec_recovery: %s command failed at %s",
+				   script,
+				   (stage == FIRST_STAGE) ? "1st stage" : "2nd stage");
+	}
+	PQclear(result);
+	pool_debug("exec_recovery: finish recovery");
+	return r;
+}
+
+/*
+ * Call pgpool_remote_start() function.
+ */
+static int exec_remote_start(PGconn *conn, BackendInfo *backend)
+{
+	PGresult *result;
+	char *hostname;
+	int r;
+
+	if (strlen(backend->backend_hostname) == 0 || *(backend->backend_hostname) == '/')
+		hostname = "localhost";
+	else
+		hostname = backend->backend_hostname;
+
+	snprintf(recovery_command, sizeof(recovery_command),
+			 "SELECT pgpool_remote_start('%s', '%s')",
+			 hostname,
+			 backend->backend_data_directory);
+
+	pool_debug("exec_remote_start: start pgpool_remote_start");
+	result = PQexec(conn, recovery_command);
+	r = (PQresultStatus(result) !=  PGRES_TUPLES_OK);
+	if (r != 0)
+		pool_error("exec_remote_start: pgpool_remote_start failed: %s", PQresultErrorMessage(result));
+	PQclear(result);
+	pool_debug("exec_remote_start: finish pgpool_remote_start");
+	return r;
+}
+
+/*
+ * Check postmaster is started.
+ */
+static int check_postmaster_started(BackendInfo *backend)
+{
+	int i = 0;
+	char port_str[16];
+	PGconn *conn;
+	char *dbname;
+
+	snprintf(port_str, sizeof(port_str),"%d", backend->backend_port);
+
+	/*
+	 * First we try with "postgres" database.
+	 */
+	dbname = "postgres";
+
+	do {
+		ConnStatusType r;
+
+		pool_log("check_postmaster_started: try to connect to postmaster on hostname:%s database:%s user:%s (retry %d times)",
+				 backend->backend_hostname, dbname, pool_config->recovery_user, i);
+
+		conn = PQsetdbLogin(backend->backend_hostname,
+							port_str,
+							NULL,
+							NULL,
+							dbname,
+							pool_config->recovery_user,
+							pool_config->recovery_password);
+
+		r = PQstatus(conn);
+		PQfinish(conn);
+		if (r == CONNECTION_OK)
+			return 0;
+
+		pool_log("check_postmaster_started: failed to connect to postmaster on hostname:%s database:%s user:%s",
+			 backend->backend_hostname, dbname, pool_config->recovery_user);
+		
+		sleep(3);
+	} while (i++ < 3);	/* XXX Hard coded retry (9 seconds) */
+
+	/*
+	 * Retry with "template1" database.
+	 */
+	dbname = "template1";
+	i = 0;
+
+	do {
+		ConnStatusType r;
+
+		pool_log("check_postmaster_started: try to connect to postmaster on hostname:%s database:%s user:%s (retry %d times)",
+				 backend->backend_hostname, dbname, pool_config->recovery_user, i);
+
+		conn = PQsetdbLogin(backend->backend_hostname,
+							port_str,
+							NULL,
+							NULL,
+							dbname,
+							pool_config->recovery_user,
+							pool_config->recovery_password);
+
+		r = PQstatus(conn);
+		PQfinish(conn);
+		if (r == CONNECTION_OK)
+			return 0;
+
+		pool_log("check_postmaster_started: failed to connect to postmaster on hostname:%s database:%s user:%s",
+			 backend->backend_hostname, dbname, pool_config->recovery_user);
+
+		if (WAIT_RETRY_COUNT != 0)
+			sleep(3);
+	} while (i++ < WAIT_RETRY_COUNT);
+
+	pool_error("check_postmaster_started: remote host start up did not finish in %d sec.", pool_config->recovery_timeout);
+	return 1;
+}
+
+static PGconn *connect_backend_libpq(BackendInfo *backend)
+{
+	char port_str[16];
+	PGconn *conn;
+
+	snprintf(port_str, sizeof(port_str),
+			 "%d", backend->backend_port);
+	conn = PQsetdbLogin(backend->backend_hostname,
+						port_str,
+						NULL,
+						NULL,
+						"template1",
+						pool_config->recovery_user,
+						pool_config->recovery_password);
+
+	if (PQstatus(conn) != CONNECTION_OK)
+	{
+		PQfinish(conn);
+		return NULL;
+	}
+	return conn;
+}
+
+/*
+ * Wait all connections are closed.
+ */
+int wait_connection_closed(void)
+{
+	int i = 0;
+
+	do {
+
+		if (Req_info->conn_counter == 0)
+			return 0;
+
+		if (WAIT_RETRY_COUNT != 0)
+			sleep(3);
+	} while (i++ < WAIT_RETRY_COUNT);
+
+	pool_error("wait_connection_closed: existing connections did not close in %d sec.", pool_config->recovery_timeout);
+	return 1;
+}
---
+++ src/watchdog/wd_lifecheck.c
@@ -0,0 +1,452 @@
+/*
+ * $Header$
+ *
+ * Handles watchdog connection, and protocol communication with pgpool-II
+ *
+ * pgpool: a language independent connection pool server for PostgreSQL
+ * written by Tatsuo Ishii
+ *
+ * Copyright (c) 2003-2013	PgPool Global Development Group
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby
+ * granted, provided that the above copyright notice appear in all
+ * copies and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of the
+ * author not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. The author makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ */
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <ctype.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <netdb.h>
+#include "pool.h"
+#include "pool_config.h"
+#include "watchdog/watchdog.h"
+#include "watchdog/wd_ext.h"
+
+#include "libpq-fe.h"
+
+int is_wd_lifecheck_ready(void);
+int wd_lifecheck(void);
+int wd_ping_pgpool(WdInfo * pgpool);
+static void * thread_ping_pgpool(void * arg);
+static PGconn * create_conn(char * hostname, int port);
+static int pgpool_down(WdInfo * pool);
+
+static void check_pgpool_status(void);
+static void check_pgpool_status_by_query(void);
+static void check_pgpool_status_by_hb(void);
+static int ping_pgpool(PGconn * conn);
+static int is_parent_alive(void);
+
+int
+is_wd_lifecheck_ready(void)
+{
+	int rtn = WD_OK;
+	WdInfo * p = WD_List;
+	int i = 0;
+
+	while (p->status != WD_END)
+	{
+		/* query mode */
+		if (!strcmp(pool_config->wd_lifecheck_method, MODE_QUERY))
+		{
+			if (wd_ping_pgpool(p) == WD_NG)
+			{
+				pool_debug("is_wd_lifecheck_ready: pgpool %d (%s:%d) has not started yet",
+				           i, p->hostname, p->pgpool_port);
+				rtn = WD_NG;
+			}
+		}
+		/* heartbeat mode */
+		else if (!strcmp(pool_config->wd_lifecheck_method, MODE_HEARTBEAT))
+		{
+			if (p == WD_List)
+			{
+				p++;
+				i++;
+				continue;
+			}
+
+			if (!WD_TIME_ISSET(p->hb_last_recv_time) ||
+			    !WD_TIME_ISSET(p->hb_send_time))
+			{
+				pool_debug("is_wd_lifecheck_ready: pgpool %d (%s:%d) has not send the heartbeat signal yet",
+				           i, p->hostname, p->pgpool_port);
+				rtn = WD_NG;
+			}
+		}
+		/* otherwise */
+		else
+		{
+			pool_error("is_wd_lifecheck_ready: unkown watchdog mode %s",
+			           pool_config->wd_lifecheck_method);
+			return WD_NG;
+		}
+
+		p ++;
+		i ++;
+	}
+
+	return rtn;
+}
+
+/*
+ * Check if pgpool is living
+ */
+int
+wd_lifecheck(void)
+{
+	struct timeval tv;
+
+	/* set startup time */
+	gettimeofday(&tv, NULL);
+
+	/* check upper connection */
+	if (strlen(pool_config->trusted_servers) &&
+		wd_is_upper_ok(pool_config->trusted_servers) != WD_OK)
+	{
+		pool_error("wd_lifecheck: failed to connect to any trusted servers");
+
+		/* This server connection may be downed */
+		if (WD_MYSELF->status == WD_MASTER)
+		{
+			wd_IP_down();
+		}
+		wd_set_myself(&tv, WD_DOWN);
+		wd_notice_server_down();
+		return WD_NG;
+	}
+
+	/* skip lifecheck during recovery execution */
+	if (*InRecovery != RECOVERY_INIT)
+	{
+		return WD_OK;
+	}
+
+	/* check pgpool status */
+	check_pgpool_status();
+
+	/* I'm in down.... */
+	if (WD_MYSELF->status == WD_DOWN)
+	{
+		pool_error("wd_lifecheck: watchdog status is DOWN. You need to restart this for recovery.");
+	}
+
+	return WD_OK;
+}
+
+/*
+ * check pgpool status and modify WDList
+ */
+static void
+check_pgpool_status()
+{
+	/* query mode */
+	if (!strcmp(pool_config->wd_lifecheck_method, MODE_QUERY))
+	{
+		check_pgpool_status_by_query();
+	}
+	/* heartbeat mode */
+	else if (!strcmp(pool_config->wd_lifecheck_method, MODE_HEARTBEAT))
+	{
+		check_pgpool_status_by_hb();
+	}
+}
+
+static void
+check_pgpool_status_by_hb(void)
+{
+	int cnt;
+	WdInfo * p = WD_List;
+	int interval;
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+
+	cnt = 0;
+	while (p->status != WD_END)
+	{
+		pool_debug("check_pgpool_status_by_hb: checking pgpool %d (%s:%d)",
+		           cnt, p->hostname, p->pgpool_port);
+
+		if (p == WD_MYSELF)
+		{
+			if (is_parent_alive() == WD_NG && WD_MYSELF->status != WD_DOWN)
+			{
+				pool_debug("check_pgpool_status_by_hb: NG; the main pgpool process does't exist.");
+				pool_log("check_pgpool_status_by_hb: lifecheck failed. pgpool %d (%s:%d) seems not to be working",
+		                 cnt, p->hostname, p->pgpool_port);
+				wd_set_myself(&tv, WD_DOWN);
+				wd_notice_server_down();
+			}
+			else
+			{
+				pool_debug("check_pgpool_status_by_hb: OK; status %d", p->status);
+			}
+		}
+		else
+		{
+			interval = WD_TIME_DIFF_SEC(tv, p->hb_last_recv_time);
+
+			if (interval > pool_config->wd_heartbeat_deadtime)
+			{
+				pool_debug("check_pgpool_status_by_hb: the latest heartbeat received %d seconds ago", interval);
+				pool_debug("check_pgpool_status_by_hb: NG; status %d", p->status);
+
+				pool_log("check_pgpool_status_by_hb: lifecheck failed. pgpool %d (%s:%d) seems not to be working",
+		                 cnt, p->hostname, p->pgpool_port);
+				pgpool_down(p);
+			}
+			else
+			{
+				pool_debug("check_pgpool_status_by_hb: the latest heartbeat received %d secconds ago", interval);
+				pool_debug("check_pgpool_status_by_hb: OK; status %d", p->status);
+			}
+		}
+
+		p++;
+		cnt++;
+		if (cnt >= MAX_WATCHDOG_NUM)
+		{
+			pool_error("check_pgpool_status_by_hb: pgpool num is out of range(%d)",cnt);
+			break;
+		}
+	}
+}
+
+static void
+check_pgpool_status_by_query(void)
+{
+	WdInfo * p = WD_List;
+	struct timeval tv;
+	pthread_attr_t attr;
+	pthread_t thread[MAX_WATCHDOG_NUM];
+	WdPgpoolThreadArg thread_arg[MAX_WATCHDOG_NUM];
+	int rc;
+	int i,cnt;
+
+	/* set startup time */
+	gettimeofday(&tv, NULL);
+
+	/* thread init */
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+	/* send packet to all watchdogs */
+	cnt = 0;
+	while (p->status != WD_END)
+	{
+		thread_arg[cnt].conn = create_conn(p->hostname, p->pgpool_port);
+		rc = pthread_create(&thread[cnt], &attr, thread_ping_pgpool, (void*)&thread_arg[cnt]);
+		p ++;
+		cnt ++;
+		if (cnt >= MAX_WATCHDOG_NUM)
+		{
+			pool_error("check_pgpool_status_by_query: pgpool num is out of range(%d)",cnt);
+			break;
+		}
+	}
+	pthread_attr_destroy(&attr);
+
+	/* check results */
+	p = WD_List;
+	for (i = 0; i < cnt; )
+	{
+		int result;
+
+		pool_debug("check_pgpool_status_by_qury: checking pgpool %d (%s:%d)",
+		           i, p->hostname, p->pgpool_port);
+
+		rc = pthread_join(thread[i], (void **)&result);
+		if ((rc != 0) && (errno == EINTR))
+		{
+			usleep(100);
+			continue;
+		}
+
+		if (result == WD_OK)
+		{
+			pool_debug("check_pgpool_status_by_query: OK; status: %d", p->status);
+
+			/* life point init */
+			p->life = pool_config->wd_life_point;
+		}
+		else
+		{
+			pool_debug("check_pgpool_status_by_query: NG; status: %d life:%d", p->status, p->life);
+
+			if (p->life > 0)
+			{
+				p->life --;
+			}
+			if (p->life <= 0)
+			{
+				pool_log("check_pgpool_status_by_query: lifecheck failed %d times. pgpool %d (%s:%d) seems not to be working",
+				         pool_config->wd_life_point, i, p->hostname, p->pgpool_port);
+
+				if ((i == 0) &&
+					(WD_MYSELF->status != WD_DOWN))
+				{
+					wd_set_myself(&tv, WD_DOWN);
+					wd_notice_server_down();
+				}
+				else
+				{
+					pgpool_down(p);
+				}
+			}
+		}
+		i++;
+		p++;
+	}
+}
+
+/*
+ * Thread function to send lifecheck query to pgpool
+ * Used in wd_lifecheck.
+ */
+static void *
+thread_ping_pgpool(void * arg)
+{
+	uintptr_t rtn;
+	WdPgpoolThreadArg * thread_arg;
+	PGconn * conn;
+
+	thread_arg = (WdPgpoolThreadArg *)arg;
+	conn = thread_arg->conn;
+	rtn = (uintptr_t)ping_pgpool(conn);	
+
+	pthread_exit((void *)rtn);
+}
+
+/*
+ * Create connection to pgpool
+ */
+static PGconn *
+create_conn(char * hostname, int port)
+{
+	static char conninfo[1024];
+	PGconn *conn;
+
+	if (strlen(pool_config->wd_lifecheck_dbname) == 0)
+	{
+		pool_error("create_conn: wd_lifecheck_dbname is empty");
+		return NULL;
+	}
+
+	if (strlen(pool_config->wd_lifecheck_user) == 0)
+	{
+		pool_error("create_conn: wd_lifecheck_user is empty");
+		return NULL;
+	}
+
+	snprintf(conninfo,sizeof(conninfo),
+		"host='%s' port='%d' dbname='%s' user='%s' password='%s' connect_timeout='%d'",
+		hostname,
+		port,
+		pool_config->wd_lifecheck_dbname,
+		pool_config->wd_lifecheck_user,
+		pool_config->wd_lifecheck_password,
+		pool_config->wd_interval / 2 + 1);
+	conn = PQconnectdb(conninfo);
+
+	if (PQstatus(conn) != CONNECTION_OK)
+	{
+		pool_debug("create_conn: Connection to database failed: %s", PQerrorMessage(conn));
+		PQfinish(conn);
+		return NULL;
+	}
+	return conn;
+}
+
+/* handle other pgpool's down */
+static int
+pgpool_down(WdInfo * pool)
+{
+	int rtn = WD_DOWN;
+
+	/* the active pgpool goes down */
+	if ((WD_MYSELF->status == WD_NORMAL) &&
+		(pool->status == WD_MASTER))
+	{
+		pool->status = WD_DOWN;
+		if (wd_am_I_oldest() == WD_OK)
+		{
+			/* stand for master */
+			rtn = wd_stand_for_master();
+			if (rtn == WD_OK)
+			{
+				/* win */
+				wd_escalation();
+			}
+		}
+	}
+	pool->status = WD_DOWN;
+	return rtn;
+}
+
+/*
+ * Check if pgpool can accept the lifecheck query.
+ */
+int
+wd_ping_pgpool(WdInfo * pgpool)
+{
+	int rtn;
+	PGconn * conn;
+
+	conn = create_conn(pgpool->hostname, pgpool->pgpool_port);
+	rtn = ping_pgpool(conn);
+
+	return rtn;
+}
+
+static int
+ping_pgpool(PGconn * conn)
+{
+	int rtn = WD_NG;
+	int status = PGRES_FATAL_ERROR;
+	PGresult * res = (PGresult *)NULL;
+
+	if (!conn)
+	{
+		return WD_NG;
+	}
+
+	res = PQexec(conn, pool_config->wd_lifecheck_query );
+
+	status = PQresultStatus(res);
+	if (res != NULL)
+	{
+		PQclear(res);
+	}
+
+	if ((status != PGRES_NONFATAL_ERROR ) &&
+		(status != PGRES_FATAL_ERROR ))
+	{
+		rtn = WD_OK;
+	}
+	PQfinish(conn);
+
+	return rtn;
+}
+
+static int
+is_parent_alive()
+{
+	if (mypid == getppid())
+		return WD_OK;
+	else
+		return WD_NG;
+}
